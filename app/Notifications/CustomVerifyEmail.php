28a75878a5d3d3e0111e81158d9f682acf852279
arsalanqayum332 <arsalanqayum332@gmail.com>
2024-04-09 05:16:20 +0500

    small fix

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -23,7 +23,7 @@ class CustomVerifyEmail extends VerifyEmail
      */
     protected function verificationUrl($notifiable)
     {
-        return config('app.frontend_url')."?email=".$notifiable->email;
+        return config('app.frontend_url')."/login?email=".$notifiable->email;
     }
 
     /**
